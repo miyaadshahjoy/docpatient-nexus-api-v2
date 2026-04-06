c9b149f0ab3a6a0e81c40da6360f75ecd728ebea
Miyaad Shah Joy <miyaadshahjoy@gmail.com>
2026-04-06 19:52:14 +0600

    CORS middleware configuration changed

--- app.js
+++ app.js
@@ -51,6 +51,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:5500',
+  'http://127.0.0.1:5500',
   'https://docpatient-nexus.onrender.com',
 ];
 
